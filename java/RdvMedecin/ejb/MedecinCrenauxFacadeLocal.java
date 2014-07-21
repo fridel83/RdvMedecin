cc2476b39ab88af66a9e7fe9a97a771288ab4c42
fridel83 <idriss.eliguen@gmail.com>
2014-07-21 21:27:46 +0200

    Mon premier commit

---
+++ java/RdvMedecin/ejb/MedecinCrenauxFacadeLocal.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package RdvMedecin.ejb;
+
+import RdvMedecin.entities.MedecinCrenaux;
+import java.util.List;
+import javax.ejb.Local;
+
+/**
+ *
+ * @author fridel
+ */
+@Local
+public interface MedecinCrenauxFacadeLocal {
+
+    void create(MedecinCrenaux medecinCrenaux);
+
+    void edit(MedecinCrenaux medecinCrenaux);
+
+    void remove(MedecinCrenaux medecinCrenaux);
+
+    MedecinCrenaux find(Object id);
+
+    List<MedecinCrenaux> findAll();
+
+    List<MedecinCrenaux> findRange(int[] range);
+
+    int count();
+    
+}
