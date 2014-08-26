f0c06d5717c01679584b6672cf5d09fe9352c4d8
fridel83 <idriss.eliguene@gmail.com>
2014-08-26 02:58:05 +0200

    new_repository

--- rdvmedecinEjb/target/generated-sources/annotations/com/rdvmedecin/rdvmedecinentities/Ville_.java
+++ rdvmedecinEjb/target/generated-sources/annotations/com/rdvmedecin/rdvmedecinentities/Ville_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.1.v20130918-rNA", date="2014-08-26T02:34:59")
+@Generated(value="EclipseLink-2.5.1.v20130918-rNA", date="2014-08-26T02:57:24")
 @StaticMetamodel(Ville.class)
 public class Ville_ { 
 
